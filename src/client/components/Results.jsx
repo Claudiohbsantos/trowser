5e241d6bcdb2fa210cfde0ae8832313794da8208
claudiohbsantos <claudiohbsantos@gmail.com>
2021-01-13 17:34:53 -0500

    center results

--- src/client/components/Results.jsx
+++ src/client/components/Results.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import ResultEntry from './ResultEntry'
 import styles from './Results.module.scss';
 
+console.log(styles)
 const Results = ({ query, searcher }) => {
   const [showType, setShowType] = useState(false)
   const [showParents, setShowParents] = useState(true)
@@ -9,7 +10,7 @@ const Results = ({ query, searcher }) => {
   const results = searcher(query);
 
   return (
-    <div className={styles.resultsContainer}>
+    <div className={styles.searchResults}>
       {results.map((entry) => ( <ResultEntry entry={entry} showType={showType} showParents={showParents}/>))}
     </div>
   );
