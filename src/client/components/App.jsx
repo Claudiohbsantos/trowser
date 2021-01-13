b2f8f35cab775191ea70c9fff3544d71faefe14d
claudiohbsantos <claudiohbsantos@gmail.com>
2021-01-13 11:50:58 -0500

    initial client setup

---
+++ src/client/components/App.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import styles from './App.module.scss';
+
+export default function App() {
+  return <h1 className={styles.happy}>Hooray</h1>;
+}
